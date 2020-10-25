02c30898317b0e114481609ec7e7974d61e8a2dd
sevar.jafarli.std@bhos.edu.az <sevar.jafarli.std@bhos.edu.az>
2020-10-25 11:26:27 +0400

    added

--- WEEK5/task1.c
+++ WEEK5/task1.c
@@ -113,11 +113,11 @@ int main()
     push(stack, 20); 
     push(stack, 30);
     push(stack, 40);
-    push(stack, 40);
-    push(stack, 40);
-    push(stack, 40);
-    push(stack, 40);
-    push(stack, 40);
+    push(stack, 50);
+    push(stack, 60);
+    push(stack, 70);
+    push(stack, 80);
+    push(stack, 90);
     pop(stack);
     pop(stack);
     pop(stack);
